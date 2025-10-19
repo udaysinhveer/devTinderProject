78bb78c5912914aacab7ec7e83e1a760e4ef4ab0
Udaysinh Veer <udaysinhveer3@gmail.com>
2025-10-19 15:14:44 +0530

    connection request model and API

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,14 +1,53 @@
 const express = require("express");
 const router = express.Router();
+const ConnectionRequest = require("../models/connectionRequests")
 
 const { userAuth } = require("../middlewares/auth");
 
-router.post("/sendConnectionRequest", userAuth, async (requestAnimationFrame, res) => {
-    const user = req.user;
-    console.log("sending a connection request");
+router.post("/request/send/:status/:toUserId",
+    userAuth,
+    async (req, res) => {
+        try {
+            const fromUserId = req.user._id;
+            const toUserId = req.params.toUserId;
+            const status = req.params.status;
 
-    res.send(user.firstName + "sent the connect request")
+            const allowedStatus = ["ignored", "interested"];
 
-});
+            if (!allowedStatus.includes(status)) {
+                return res.status(400).json({ message: "Invalid status type " + status })
+            }
+
+            // If there is an existing connection request or not 
+
+            const existingConnectionRequest = await ConnectionRequest.findOne({
+                $or: [
+                    { fromUserId, toUserId },
+                    { fromUserId: toUserId, toUserId: fromUserId }
+                ],
+            });
+            if (existingConnectionRequest) {
+                return res
+                .status(400).send({message:"Connection request already exist"})
+            }
+
+            const connectionRequest = new ConnectionRequest({
+                fromUserId,
+                toUserId,
+                status
+            });
+
+            const data = await connectionRequest.save();
+
+            res.json({
+                message: "Connection Request sent successfully",
+                data,
+            });
+
+        } catch (err) {
+            res.status(400).send("ERROR " + err.message)
+        }
+
+    });
 
 module.exports = router
